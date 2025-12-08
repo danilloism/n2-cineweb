3ffa5fdf10de10880e104c7e0d5cdf9055e642dd
Danillo Ilggner <danilloilggner@gmail.com>
2025-12-08 14:47:41 -0300

    ajuste nevagação

--- frontend/src/components/Nav/index.tsx
+++ frontend/src/components/Nav/index.tsx
@@ -2,43 +2,61 @@ import { Link } from 'react-router-dom';
 
 export const Nav = () => {
   return (
-    <>
-      <div className="d-flex justify-content-center bg-dark text-white p-3 mb-5">
-        <ul className="nav">
-          <li className="nav-item mx-3">
-            <Link
-              to="/"
-              className="nav-link text-white"
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item mx-3">
-            <Link
-              to="/filmes"
-              className="nav-link text-white"
-            >
-              Filmes
-            </Link>
-          </li>
-          <li className="nav-item mx-3">
-            <Link
-              to="/salas"
-              className="nav-link text-white"
-            >
-              Salas
-            </Link>
-          </li>
-          <li className="nav-item mx-3">
-            <Link
-              to="/sessoes"
-              className="nav-link text-white"
-            >
-              Sessoes
-            </Link>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link
+          className="navbar-brand"
+          to="/"
+        >
+          <i className="bi bi-film me-2"></i>
+          CineWeb
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarItems"
+          aria-controls="navbarItems"
+          aria-expanded="false"
+          aria-label="Expandir menu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarItems"
+        >
+          <ul className="navbar-nav mr-auto ms-auto">
+            <li className="nav-item">
+              <Link
+                to="/filmes"
+                className="nav-link text-white"
+              >
+                <i className="bi bi-film me-1"></i>
+                Filmes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/salas"
+                className="nav-link text-white"
+              >
+                <i className="bi bi-door-closed me-1"></i>
+                Salas
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sessoes"
+                className="nav-link text-white"
+              >
+                <i className="bi bi-calendar2-event me-1"></i>
+                Sessões
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </>
+    </nav>
   );
 };
